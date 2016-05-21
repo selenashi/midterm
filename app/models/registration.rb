be7b31ee276d2120b3277b6b951947ba9996bb86
selenashi <shiming.shi0718@gmail.com>
2016-05-21 20:09:45 +0000

    Link to my_event

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -4,6 +4,13 @@ class Registration < ActiveRecord::Base
 
   validates :num_tickets, presence: true
   validates :num_tickets, numericality: { only_integer: true }
-  after_create :send_sms
+  # after_create :send_sms
+
+
+  # def min_required_reached?
+  #   event = Event.find(params[:event_id])
+  #   event.num_user >= events.min_required
+  # end
+
 
 end
--- app/views/helper.rb
+++ app/views/helper.rb
@@ -4,10 +4,4 @@ helpers do
     '%.2f' % price
   end  
 
-  # def current_user
-  #   if session[:user_id]
-  #     User.find(session[:user_id])
-  #   end
-  # end
-
 end
